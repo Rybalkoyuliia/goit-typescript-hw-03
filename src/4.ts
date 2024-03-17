612daef05ad43125a95a7b737e48e75c8b6553ec
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2024-03-17 14:57:36 -0500

    finished tasks

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,51 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor(signature: number) {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  protected key: Key;
+  private tenants: Person[];
+
+  constructor(key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+  }
+}
+
+const key = new Key(3);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +53,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
